d7eefe5d371a6a7ca27d2624ca26bd682af9ffc5
Jos Hollander <jos.hollander@ogd.nl>
2018-02-19 15:07:37 +0100

    Add project files.

---
+++ FlipsTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlipsTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
